b9f18cf5f63a1532d5c55b1662ae5291f02fb38f
Hitesh Goyal <hitesh18vg@gmail.com>
2020-01-24 14:14:43 +0530

    django tutorial with CV

---
+++ accounts/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+
+def home(request):
+    # return HttpResponse("Hello!! Home Page")
+    return render(request, 'accounts/login.html')
